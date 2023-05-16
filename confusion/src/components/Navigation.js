fbe3dae473fe5579c724881c2bc5be594f29ce7f
unknown <tvkhang2905@gmail.com>
2023-05-16 22:17:03 +0700

    Functional Components

--- confusion/src/components/Navigation.js
+++ confusion/src/components/Navigation.js
@@ -1,28 +1,17 @@
 import React, { Component } from 'react'
 export default class Navigation extends Component {
-    return (
-        <nav>
-      <ul>
-          <li><a className='active' href='#home'>Home</a></li>
-          <li><a href='#news'>News</a></li>
-          <li><a href='#about'>About</a></li>
-          <li><a href='#contact'>Contact</a></li>
-      </ul>
-    </nav>
-      )
-    
-    // render() {
-    //     return (
-    //         <div className='back-ground'>
-    //             <nav>
-    //                 <ul>
-    //                     <a className='active' href='#home'>Home</a>
-    //                     <a className='active' href='#news'>News</a>
-    //                     <a className='active' href='#about'>About</a>
-    //                     <a className='active' href='#contact'>Contact</a>
-    //                 </ul>
-    //             </nav>
-    //         </div>
-    //     )
-    // }
+    render() {
+        return (
+            <div className='back-ground'>
+                <nav>
+                    <ul>
+                        <a className='active' href='#home'>Home</a>
+                        <a className='active' href='#news'>News</a>
+                        <a className='active' href='#about'>About</a>
+                        <a className='active' href='#contact'>Contact</a>
+                    </ul>
+                </nav>
+            </div>
+        )
+    }
 }
--- confusion/src/components/Players.js
+++ confusion/src/components/Players.js
@@ -1,23 +1,22 @@
-import React , { Component} from 'react'
-import { Players } from '../shared/ListOfPlayers'
-export default class player extends Component{
-    render() {
-        return (
-          <div className='container'>
-            {Players.map((player) => (
-              <div className='column'>
-                <div className='card'>
-                  <img src={player.img} />
-                  <h3>{player.name}</h3>
-                  <p className='title'>{player.club}</p>
-                  <p className="but"><button>Detail</button></p>
-                </div>
-              </div>
-            ))}
+import React, { Component } from "react";
+import { Players } from "../shared/ListOfPlayers";
+  export default function player(){
+    return (
+      <div className='container'>
+        {Players.map((player) => (
+          <div className='column'>
+            <div className='card'>
+              <img src={player.img} />
+              <h3>{player.name}</h3>
+              <p className='title'>{player.club}</p>
+              <p className="but"><button>Detail</button></p>
+            </div>
           </div>
-        )
-      }
-    }
+        ))}
+      </div>
+    )
+}
+    
 //    render(){
 //         return (
 //             <div className="container">
