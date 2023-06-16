564033ad16c1a7d69da612fda4390f87a3775ddd
unknown <tvkhang2905@gmail.com>
2023-06-16 11:05:13 +0700

    bt12

--- src/App.js
+++ src/App.js
@@ -2,16 +2,26 @@ import Navigation from './components/Navigation';
 import './App.css';
 // import Main from './components/Main';
 import Footer from './components/Footer';
-import Player from './components/Players';
+import Players from './components/Players';
+import Detail from './components/Detail';
+import {
+  Routes,
+  Route,
+} from "react-router-dom";
 function App() {
   return (
     <div className='App'>
       <Navigation/>
-      {/* <Main/> */}
-      <Player/>
+      <Routes>
+          <Route path='/' element={<Players/>}> 
+      </Route>
+        <Route path='/detail/:id' element={<Detail/>}></Route>
+        {/* <Route path='/contact' element={<Contact/>}></Route> */}
+      </Routes>
       <Footer/>
     </div>
   );
 }
+
 
 export default App;
--- src/components/Players.js
+++ src/components/Players.js
@@ -1,27 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Players } from "../shared/ListOfPlayers";
 import { useState } from "react";
+import { Link } from 'react-router-dom'
+import { ThemeContext } from "./ThemeConText";
 export default function Player(){
-  const [players, setPlayer] = useState([]);
+  const [player, setPlayer] = useState([]);
+  const {theme, toggle ,dark } = useContext(ThemeContext)
     return (
     <div className='container'>
+
         {Players.map((player)=>(
            <div className='column'>
            <div className='card'>
            <img src={player.img}/>
              <h3>{player.name}</h3>
              <p className='title'>{player.club}</p>
+             <Link to={`detail/${player.id}`}>
              <p><button onClick={()=>{setPlayer(player)}}><a href='#popup1' id='openPopUp'>Detail</a></button></p>
+           </Link>
            </div>
          </div>
         ))}
         <div id ='popup1' className='overlay'>
           <div className='popup'>
-            <img src={players.img}/>
-            <h2>{players.name}</h2>
+            <img src={player.img}/>
+            <h2>{player.name}</h2>
             <a className='close' href='#'>&times;</a>
             <div className='content'>
-              {players.info}
+              {player.info}
             </div>
           </div>
         </div>
